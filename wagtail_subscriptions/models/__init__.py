72e12a17c58734b7f18930e40f010afa88deb49b
Md Mojno Miya <113609493+mojnomiya@users.noreply.github.com>
2026-02-10 21:23:23 +0600

    fix: Export models from __init__.py to fix ImportError

---
+++ wagtail_subscriptions/models/__init__.py
@@ -0,0 +1,14 @@
+from .core import Customer, Subscription, SubscriptionPlan
+from .features import Feature, Module, PlanFeature
+from .permissions import SubscriptionGroup, SubscriptionPermission
+
+__all__ = [
+    "Customer",
+    "Subscription",
+    "SubscriptionPlan",
+    "Feature",
+    "Module",
+    "PlanFeature",
+    "SubscriptionGroup",
+    "SubscriptionPermission",
+]
