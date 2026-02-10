17bce39ee460458e035f1f589a6ec6722fb9474f
Md Mojno Miya <113609493+mojnomiya@users.noreply.github.com>
2026-02-10 21:27:36 +0600

    fix: Export views from __init__.py

---
+++ wagtail_subscriptions/views/__init__.py
@@ -0,0 +1,10 @@
+from .analytics import AnalyticsAPIView
+from .api import PricingPlansAPIView, SubscriptionAPIView
+from .pricing import PricingView
+
+__all__ = [
+    "AnalyticsAPIView",
+    "PricingPlansAPIView",
+    "PricingView",
+    "SubscriptionAPIView",
+]
